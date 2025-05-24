575500ffcb92c2a667ce07e9d7277e3885686612
aditya20014 <adistarted@gmail.com>
2025-05-24 17:24:36 +0000

    this is sum 2 number

---
+++ Variables/bool.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+// booleans - outout in true and false only
+bool isSafe = false;
+cout<< isSafe << endl;
+//true->1, false->0
+    return 0;
+}
