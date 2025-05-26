11800d1e5806f4bebdfe9a230e845c350dd162f9
aditya20014 <adistarted@gmail.com>
2025-05-26 06:00:38 +0000

    upper and lower

--- conditional & LOOP/case.cpp
+++ conditional & LOOP/case.cpp
@@ -4,6 +4,8 @@ int main(){
   char ch;
   cout<<"enter ch: ";
   cin>>ch;
+//ASCII number (a-z)=(97 to 122)
+// ASCII  number (A-Z)=(65 to 90)
   if (ch>='a' && ch<='z')
   {
     cout<<"lower case\n";
